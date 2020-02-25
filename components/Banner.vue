4a5a55928348be8831c96b674dac8986de1eaa55
SosrG <61453295+SosrG@users.noreply.github.com>
2020-02-25 15:32:50 +0530

    Update Banner.vue

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="container-fluid welcome p-4 mt-5">
       <div class="desc">
-        <h1 class="display-4 text-white">Want to connect with people</h1>
+        <h1 class="display-4 text-white">Connect to Create your Artist Account</h1>
         <p class="lead text-white">
           Enter into the world of artists, where they connect to grow and share their experiences.
         </p>
